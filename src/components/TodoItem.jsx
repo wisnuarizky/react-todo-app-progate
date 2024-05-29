18b8794cbd6032627c72af8a3705544ad9698449
wisnuarizky <wisnuarizky01@gmail.com>
2024-05-29 09:05:07 +0700

    menambahkan css

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable react/prop-types */
 function TodoItem({ todo }) {
-  return <p>{todo.title}</p>;
+  const style = {
+    border: '2px solid #f4f4f4',
+    fontSize: '24px',
+  };
+
+  return <p style={style}>{todo.title}</p>;
 }
 
 export default TodoItem;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,9 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
 import TodoItem from './TodoItem';
 
 function Todos({ todos }) {
   return (
-    <div>
-      <h1>my todos list</h1>
+    <div style={{ width: '40%', margin: '0 auto' }}>
       {todos.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />;
       })}
